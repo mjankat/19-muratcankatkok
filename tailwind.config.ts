2ef01919d80f1ec7b44248f61a63f1a2f9c24b18
MURAT CANKAT KÖK <mcankatkok@gmail.com>
2024-01-05 21:02:27 +0300

    firstcommit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,11 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        customGray: '#D8D8D8',
+        inputCustomGray: '#F8F8F8'
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 } satisfies Config;
